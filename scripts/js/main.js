8b8e483942dad8e94e022111bb03e335fb410239
DESKTOP-RB65TLM\xutur <xuturk@hotmail.com>
2017-12-10 17:55:12 +0100

    Update

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -2,7 +2,6 @@
 var virtualClick = false;
 
 $(document).ready(function () {
-    hideContent();
     //Subscribe to click events in menu
     $(".navigation ul li a[href^='#']").click(function (e) {
 
@@ -14,47 +13,25 @@ $(document).ready(function () {
         var parent = this;
 
         if (hash == "#AvoidTheWalls" && currenthash != "#AvoidTheWalls") {
-            hideContent();
             $("main").load('scripts/html/avoidthewalls.html', function () {
                 $("main").removeClass("fill");
                 finishChangePage(hash, parent);
                 showContent();
             });
         }
-        else if (hash == "#Blog" && currenthash != "#Blog") {
-            hideContent();
-            $("main").load('scripts/html/blog.html', function () {
-                finishChangePage(hash, parent);
-                showContent();
-            });
-        }
         else {
-            if (currenthash == "#AvoidTheWalls" || currenthash == "#Blog") {
-                hideContent();
+            if (currenthash == "#AvoidTheWalls" ) {
                 //Si estamos fuera de la home, la recargamos.
                 $("main").load('scripts/html/main.html', function () {
-                    animationAnchor(hash, parent);
                     $("main").addClass("fill");
                     showContent(processUrl);
                 });
             }
-            else {
-                animationAnchor(hash, parent);
-            }
         }
     });
 
-    //hide the mobile menu when click in option:
-    $("#mobile-bar a").click(function () {
-        if (!virtualClick)
-            $("#navbarBtnToggle").click();
-
-        virtualClick = false;
-    });
-
     $(".linkhome").addClass("active");
-
-
+    
     //Load the main page.
     $("main").load('scripts/html/main.html', function () {
         showContent(processUrl);
@@ -64,19 +41,6 @@ $(document).ready(function () {
 
 function showContent(callback) {
     $.when($("main").fadeIn(400)).done(function () { if (typeof callback === "function") { callback(); } });
-}
-
-function hideContent() {
-  //  $("main").hide();
-}
-
-function animationAnchor(hash, parent) {
-    // animate
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top
-    }, 300, function () {
-        finishChangePage(hash, parent);
-    });
 }
 
 function finishChangePage(hash, parent) {
